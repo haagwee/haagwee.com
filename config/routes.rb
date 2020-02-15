d2152bdf56327af5d6eddd6424d6c02401e57e11
Dunya Kirkali <dunyakirkali@gmail.com>
2020-02-15 16:29:33 +0100

    Disable registration

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,10 @@ Rails.application.routes.draw do
   resources :projects
   resources :posts
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :users
+  devise_for :users, :skip => [:registrations], controllers: {
+    sessions: 'users/sessions'
+  }
+
   get 'welcome/index'
 
   root 'welcome#index'
