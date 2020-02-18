8bc0e99a25c19bdc07106c1b9cc122d48fbc928b
Dunya Kirkali <dunyakirkali@gmail.com>
2020-02-18 08:18:10 +0100

    Use bulma

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  resources :projects
-  resources :posts
+  resources :projects, only: [:show, :index]
+  resources :posts, only: [:show, :index]
+
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, :skip => [:registrations]
 
