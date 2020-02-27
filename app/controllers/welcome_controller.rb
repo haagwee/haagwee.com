52fc88eb52c499b589ee520c9f02507b1a3d3671
Dunya Kirkali <dunyakirkali@gmail.com>
2020-02-27 08:43:20 +0100

    Add sample analytics

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    Analytics.page(name: 'Home')
   end
 end
--- config/initializers/analytics_ruby.rb
+++ config/initializers/analytics_ruby.rb
@@ -1,4 +1,1 @@
-Analytics = Segment::Analytics.new({
-    write_key: ENV['SEGMENT_KEY'],
-    on_error: Proc.new { |status, msg| print msg }
-})
+Analytics = Segment::Analytics.new(write_key: ENV['SEGMENT_KEY'])
